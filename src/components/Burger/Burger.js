84a2252fe0df517013ff1dbce0350b08a0865249
MaxKichiy <6aJIa6oJI1336@gmail.com>
2020-04-30 17:59:40 +0300

    unpuck objects and make burger depends from state

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -3,11 +3,20 @@ import ss from './Burger.module.css';
 import BurgetIngridient from './BurgerIngridient/BurgerIngridient';
 
 const Burger = (props) => {
+  let objKeys = Object.keys(props.ingredients); // возвращаем список ключей с обьекта
+
+  let ingArray = [];
+  objKeys.map((e, index) => {
+    for (let i = 0; i < props.ingredients[e]; i++) {
+      //проходим по каждому с ключей берем
+      ingArray.push(<BurgetIngridient type={e} key={e + i + index} />);
+    }
+  });
+
   return (
     <div className={ss.Burger}>
       <BurgetIngridient type='bread-top' />
-      <BurgetIngridient type='cheese' />
-      <BurgetIngridient type='meat' />
+      {ingArray}
       <BurgetIngridient type='bread-bottom' />
     </div>
   );
--- src/components/Burger/BurgerIngridient/BurgerIngridient.js
+++ src/components/Burger/BurgerIngridient/BurgerIngridient.js
@@ -3,13 +3,13 @@ import ss from './BurgerIngridient.module.css';
 import PropTypes from 'prop-types';
 
 const BurgetIngridient = (props) => {
-  let ingridient = null;
+  let ingredient = null;
   switch (props.type) {
     case 'bread-bottom':
-      ingridient = <div className={ss.BreadBottom}></div>;
+      ingredient = <div className={ss.BreadBottom}></div>;
       break;
     case 'bread-top':
-      ingridient = (
+      ingredient = (
         <div className={ss.BreadTop}>
           <div className={ss.Seeds1}></div>
           <div className={ss.Seeds2}></div>
@@ -17,22 +17,22 @@ const BurgetIngridient = (props) => {
       );
       break;
     case 'meat':
-      ingridient = <div className={ss.Meat}></div>;
+      ingredient = <div className={ss.Meat}></div>;
       break;
     case 'cheese':
-      ingridient = <div className={ss.Cheese}></div>;
+      ingredient = <div className={ss.Cheese}></div>;
       break;
     case 'salad':
-      ingridient = <div className={ss.Salad}></div>;
+      ingredient = <div className={ss.Salad}></div>;
       break;
     case 'bacon':
-      ingridient = <div className={ss.Bacon}></div>;
+      ingredient = <div className={ss.Bacon}></div>;
       break;
     default:
-      ingridient = null;
+      ingredient = null;
   }
 
-  return ingridient;
+  return ingredient;
 };
 
 BurgetIngridient.propTypes = {
