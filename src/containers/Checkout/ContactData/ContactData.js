43360b17236eb94997ab2b0207cfce18392bfd6b
MaxKichiy <6aJIa6oJI1336@gmail.com>
2020-07-05 18:27:01 +0300

    add a login form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -125,8 +125,18 @@ const ContactData = (props) => {
   const checkValidity = (value, rules) => {
     let isValid = true;
 
-    if (rules.required && isValid) {
-      isValid = value.trim() !== '';
+    if (rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if (rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if (rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    if (rules.isNumeric) {
+      const pattern = /^\d+$/;
+      isValid = pattern.test(value) && isValid;
     }
 
     return isValid;
