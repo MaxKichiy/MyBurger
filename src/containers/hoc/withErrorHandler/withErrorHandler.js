41a7fd9e688c0323ecf8fe52d65b9518fad7a0e5
MaxKichiy <6aJIa6oJI1336@gmail.com>
2020-07-22 09:36:36 +0300

    add a custom hook

--- src/containers/hoc/withErrorHandler/withErrorHandler.js
+++ src/containers/hoc/withErrorHandler/withErrorHandler.js
@@ -1,40 +1,16 @@
 import React, { useState } from 'react';
 import Modal from '../../../components/UI/Modal/Modal';
 import { useEffect } from 'react';
+import useHttpErrorHandler from '../../../hooks/http-error-handler';
 
 const withErrorHandler = (WrappedComponent, axios) => {
   return (props) => {
-    const [myError, setError] = useState(null);
-
-    const reqInterceptor = axios.interceptors.request.use((req) => {
-      setError(null);
-      return req;
-    });
-    const resInterceptor = axios.interceptors.response.use(
-      (res) => res,
-      (err) => {
-        setError(err);
-      }
-    );
-
-    useEffect(() => {
-      return () => {
-        axios.interceptors.request.eject(reqInterceptor);
-        axios.interceptors.response.eject(resInterceptor);
-      };
-    }, [reqInterceptor, resInterceptor]);
-    // axios.interceptors.response.use(null, (error) => {
-    //   setError(error);
-    // });
-
-    const errorConfirmedHandler = () => {
-      setError(null);
-    };
+    const [error, clearError] = useHttpErrorHandler(axios);
 
     return (
       <React.Fragment>
-        <Modal show={myError} hide={errorConfirmedHandler}>
-          {myError ? myError.message : null}
+        <Modal show={error} hide={clearError}>
+          {error ? error.message : null}
         </Modal>
         <WrappedComponent {...props} />
       </React.Fragment>
